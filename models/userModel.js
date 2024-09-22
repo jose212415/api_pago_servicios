ed4de520fae73c77fcebb0f39eb2153e1c2e1989
jose152021 <115026906+jose212415@users.noreply.github.com>
2024-09-22 09:46:41 -0700

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -10,6 +10,6 @@ const userSchema = new mongoose.Schema({
   role: { type: String, default: "user" },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
-});
+}, { collection: 'User' }););
 
 module.exports = mongoose.model("User", userSchema);
